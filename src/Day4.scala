54d11a98d624b4ada90021208db1396257b8b260
kasparito <kaspar@hellden.com>
2019-12-04 21:43:19 +0100

    day 4 results

--- src/Day4.scala
+++ src/Day4.scala
@@ -14,7 +14,7 @@ object Day4 extends Base(4) {
       .sliding(2)
       .forall { case Seq(x, y) => x <= y }
 
-  override def part1: Int =
+  override def part1: Int = // 931
     passwordRange.count(p => adjacentDigits(p) & neverDecrease(p))
 
   def soloAdjacentDigits(password: String): Boolean =
@@ -23,6 +23,6 @@ object Day4 extends Base(4) {
       .sliding(4)
       .exists { case Seq(pre, x, y, post) => pre != x && x == y && y != post }
 
-  override def part2: Int =
+  override def part2: Int = // 609
     passwordRange.count(p => soloAdjacentDigits(p) & neverDecrease(p))
 }
