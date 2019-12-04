2c6623df098ac510149f0edd68fede40e68b4a65
kasparito <kaspar@hellden.com>
2019-12-04 21:39:50 +0100

    day 4

---
+++ src/Day4.scala
@@ -0,0 +1,28 @@
+object Day4 extends Base(4) {
+
+  val passwordRange: Seq[String] = (272091 to 815432).map(_.toString)
+
+  def adjacentDigits(password: String): Boolean =
+    password
+      .iterator
+      .sliding(2)
+      .exists { case Seq(x, y) => x == y }
+
+  def neverDecrease(password: String): Boolean =
+    password
+      .iterator
+      .sliding(2)
+      .forall { case Seq(x, y) => x <= y }
+
+  override def part1: Int =
+    passwordRange.count(p => adjacentDigits(p) & neverDecrease(p))
+
+  def soloAdjacentDigits(password: String): Boolean =
+    (" " + password + " ")
+      .iterator
+      .sliding(4)
+      .exists { case Seq(pre, x, y, post) => pre != x && x == y && y != post }
+
+  override def part2: Int =
+    passwordRange.count(p => soloAdjacentDigits(p) & neverDecrease(p))
+}
