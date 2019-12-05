2b13e2ac26cb3c81a0ee2d1d3e5f290da6eeb29a
kasparito <kaspar@hellden.com>
2019-12-05 08:46:07 +0100

    day 5 cleanup

--- src/Day5.scala
+++ src/Day5.scala
@@ -2,74 +2,50 @@ object Day5 extends Base(5) {
 
   def initialState: Array[Int] = inputLines.head.split(',').map(_.toInt)
 
-  def run(state: Array[Int], input: Int): Int = {
+  class IntcodeComputer(state: Array[Int]) {
 
     @scala.annotation.tailrec
-    def run(position: Int, input: Int): Int = {
-      val operation = state(position).toString
-      operation.takeRight(2).toInt match {
+    private def run(position: Int, input: Int): Int = {
+      val instruction = state(position).toString
+      val operation = instruction.takeRight(2).toInt
+      val termTypes = instruction.dropRight(2).reverse.map(_.toString.toInt).padTo(2, 0)
+      def term(i: Int): Int = if (termTypes(i - 1) == 1) state(position + i) else state(state(position + i))
+      def set(i: Int, v: Int): Unit = state(state(position + i)) = v
+
+      operation match {
         case 1 =>
-          val Array(term1, term2, result) = state.slice(position + 1, position + 4)
-          val Seq(termType1, termType2) = operation.dropRight(2).reverse.map(_.toString.toInt).padTo(2, 0)
-          def term(v: Int, t: Int) = if (t == 1) v else state(v)
-          state(result) = term(term1, termType1) + term(term2, termType2)
+          set(3, term(1) + term(2))
           run(position + 4, input)
         case 2 =>
-          val Array(term1, term2, result) = state.slice(position + 1, position + 4)
-          val Seq(termType1, termType2) = operation.dropRight(2).reverse.map(_.toString.toInt).padTo(2, 0)
-          def term(v: Int, t: Int) = if (t == 1) v else state(v)
-          state(result) = term(term1, termType1) * term(term2, termType2)
+          set(3, term(1) * term(2))
           run(position + 4, input)
         case 3 =>
-          val result = state(position + 1)
-          state(result) = input
+          set(1, input)
           run(position + 2, input)
         case 4 =>
-          val result = state(position + 1)
-          run(position + 2, state(result))
+          run(position + 2, term(1))
         case 5 =>
-          val Array(term1, term2) = state.slice(position + 1, position + 3)
-          val Seq(termType1, termType2) = operation.dropRight(2).reverse.map(_.toString.toInt).padTo(2, 0)
-          def term(v: Int, t: Int) = if (t == 1) v else state(v)
-          val newPosition =
-            if (term(term1, termType1) != 0)
-              term(term2, termType2)
-            else
-              position + 3
-          run(newPosition, input)
+          run(if (term(1) != 0) term(2) else position + 3, input)
         case 6 =>
-          val Array(term1, term2) = state.slice(position + 1, position + 3)
-          val Seq(termType1, termType2) = operation.dropRight(2).reverse.map(_.toString.toInt).padTo(2, 0)
-          def term(v: Int, t: Int) = if (t == 1) v else state(v)
-          val newPosition =
-            if (term(term1, termType1) == 0)
-              term(term2, termType2)
-            else
-              position + 3
-          run(newPosition, input)
+          run(if (term(1) == 0) term(2) else position + 3, input)
         case 7 =>
-          val Array(term1, term2, result) = state.slice(position + 1, position + 4)
-          val Seq(termType1, termType2) = operation.dropRight(2).reverse.map(_.toString.toInt).padTo(2, 0)
-          def term(v: Int, t: Int) = if (t == 1) v else state(v)
-          state(result) = if (term(term1, termType1) < term(term2, termType2)) 1 else 0
+          set(3, if (term(1) < term(2)) 1 else 0)
           run(position + 4, input)
         case 8 =>
-          val Array(term1, term2, result) = state.slice(position + 1, position + 4)
-          val Seq(termType1, termType2) = operation.dropRight(2).reverse.map(_.toString.toInt).padTo(2, 0)
-          def term(v: Int, t: Int) = if (t == 1) v else state(v)
-          state(result) = if (term(term1, termType1) == term(term2, termType2)) 1 else 0
+          set(3, if (term(1) == term(2)) 1 else 0)
           run(position + 4, input)
         case 99 =>
           input
       }
     }
 
-    run(0, input)
+    def run(input: Int): Int =
+      run(0, input)
   }
 
   override def part1: Int = // 13087969
-    run(initialState, 1)
+    new IntcodeComputer(initialState).run(1)
 
   override def part2: Int = // 14110739
-    run(initialState, 5)
+    new IntcodeComputer(initialState).run(5)
 }
