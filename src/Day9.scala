498cc0d6c9dae7c4a460b9ad845684a350220f7c
kasparito <kaspar@hellden.com>
2019-12-14 00:47:10 +0100

    day 9 part 2

--- src/Day9.scala
+++ src/Day9.scala
@@ -2,6 +2,9 @@ object Day9 extends Base(9) {
 
   def initialState: Array[Long] = inputLines.head.split(',').map(_.toLong)
 
-  override def part1: Long = //
+  override def part1: Long = // 3454977209
     IntcodeComputer.run(initialState, new SimpleHandler(Seq(1))).await.head
+
+  override def part2: Long = // 50120
+    IntcodeComputer.run(initialState, new SimpleHandler(Seq(2))).await.head
 }
